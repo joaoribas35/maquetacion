f515a197ecd72717c272e53b9f452615059916b5
Joao Manoel Ribas <joaoribas35@hotmail.com>
2021-08-31 16:41:08 -0300

    create modal

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("modal");
+const openModal = document.getElementById("submit-modal");
+const closeModal = document.getElementById("close");
+
+openModal.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeModal.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
